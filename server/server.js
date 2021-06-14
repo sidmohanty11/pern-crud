550ea0fe9e2b7cf68798a3379a516e5380bb6fe5
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-06-15 00:32:14 +0530

    laying out the api structure

--- server/server.js
+++ server/server.js
@@ -1,7 +1,42 @@
+require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(morgan("dev"));
+app.use(express.json());
+//get all restaurants
+app.get('/api/v1/restaurants', (req, res) => {
+    res.status(200).json({
+            status: "success",
+        data: {
+            restaurant: ["mcD", "wendys", "pizzahut", "kfc"],
+        }
+        })
+});
+
+// get a particular restaurant
+app.get('/api/v1/restaurants/:id', (req, res) => {
+    const { id } = req.params;
+});
+
+//create a restaurant
+app.post('/api/v1/restaurants', (req, res) => {
+    console.log(`req.body`, req.body)
+});
+
+//update a restaurant
+app.put('/api/v1/restaurants/:id', (req, res) => {
+    const { id } = req.params;
+});
+
+//delete a restaurant
+app.delete('/api/v1/restaurants/:id', (req, res) => {
+    const { id } = req.params;
+});
+
+//fire up an express server
 app.listen(PORT, () => {
-    console.log('listening at port:',PORT)
-})
+    console.log('listening at port:', PORT)
+});
